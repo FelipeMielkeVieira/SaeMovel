4b511653468aa7d3e106337795fa2a6ccb12c30a
Felipe Vieira <felipe_mielke-vieira@estudante.sc.senai.br>
2022-11-24 11:10:41 -0300

    24/11 - 11:10

--- front-end/src/app/modal-area/modal-area.component.ts
+++ front-end/src/app/modal-area/modal-area.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AutomovelService } from '../service/automovelService';
 
 @Component({
   selector: 'app-modal-area',
@@ -8,9 +9,16 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ModalAreaComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { area: number }) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { area: number }, private automovelService: AutomovelService) { }
 
   ngOnInit(): void {
+    this.automovelService.getByArea(this.data.area).subscribe(
+      data => { this.listaAutomoveis = data; }
+    );
+  }
+
+  formatPreco(valor: number) {
+    return valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
   }
 
   listaAutomoveis: any = [{ id: 1, modelo: 'Gol', preco: 10000 }, { id: 2, modelo: 'Palio', preco: 12000 }, { id: 3, modelo: 'Uno', preco: 8000 }];

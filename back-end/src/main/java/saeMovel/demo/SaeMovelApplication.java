672a950a92d3bd2afd549e5cd85567c900564dce
Felipe Vieira <felipe_mielke-vieira@estudante.sc.senai.br>
2022-11-24 07:58:34 -0300

    a
    a

---
+++ back-end/src/main/java/saeMovel/demo/SaeMovelApplication.java
@@ -0,0 +1,13 @@
+package saeMovel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeMovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaeMovelApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/saeMovel/demo/SaeMovelApplicationTests.java
@@ -0,0 +1,13 @@
+package saeMovel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaeMovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
